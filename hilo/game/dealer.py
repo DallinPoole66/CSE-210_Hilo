079a8fb5941f1b98d22a4dacca3166982d3bb3c5
ctfuller98 <ful17005@byui.edu>
2021-06-15 16:31:26 -0600

    dealer complete

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -4,10 +4,16 @@ class Dealer:
     Represents the dealer in the game.
     Responsible for drawing a card and showing it.
     Responsible for showing the current card.
-    Attributes: last_card 
+    Attributes: last_card, current_card
     '''
     def __init__(self):
         self.last_card = random.randint(1,13)
+        self.current_card = self.last_card
     def draw_card(self):
-        card = random.randint(1,13)
-        return card
+        self.last_card = self.current_card
+        self.current_card = random.randint(1,13)
+        return self.current_card
+    def get_last_card(self):
+        return self.last_card
+    def get_next_card(self):
+        return self.current_card
